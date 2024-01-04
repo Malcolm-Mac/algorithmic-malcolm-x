d03950def44f133576cba555ebcdd9274e7951dc
Malcolm453 <malcolm@vetro.co.za>
2024-01-04 18:25:44 +0200

    quick fixes

--- app/(landing)/page.js
+++ app/(landing)/page.js
@@ -11,14 +11,14 @@ export default function Home() {
   
   return (
     <main>
-      <section className="flex items-center h-screen relative px-[30px] lg:px-[60px]">
+      <section className="flex items-center h-screen relative px-[10px] lg:px-[60px]">
         <Header />
         <h1 className="overflow-hidden inline-block text-heading font-bold text-[45px] leading-[100%] xl:text-[120px] 2xl:text-[150px] xl:leading-[120px] 2xl:leading-[140px] uppercase mb-16 text-white">
           Fullstack <br />
-          <span className="ml-10 whitespace-nowrap relative inline-block">Developer</span>
+          <span className="ml-6 lg:ml-10 whitespace-nowrap relative inline-block">Developer</span>
         </h1>
         <Badges />
-        <div className="absolute left-0 bottom-[60px] right-0 flex items-end lg:items-start justify-between px-[30px] lg:px-[60px]">
+        <div className="absolute left-0 bottom-[60px] right-0 flex items-end lg:items-start justify-between px-[10px] lg:px-[60px]">
           <div className="flex items-center">
             <RotatingText />
           </div>
@@ -28,7 +28,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="flex items-center h-screen relative px-[30px] lg:px-[60px]">
+      <section className="flex items-center h-screen relative px-[10px] lg:px-[60px]">
         <Insights />
       </section>
 
--- components/Availability.jsx
+++ components/Availability.jsx
@@ -4,9 +4,9 @@ const Availability = () => {
   return (
     <div className="flex flex-col md:mb-2 lg:mt-10 lg:mb-0 mb-10">
       <div
-        className="flex items-center gap-4 xl:gap-[20px] 2xl:gap-[30px] down"
+        className="flex items-center gap-4 xl:gap-[20px] 2xl:gap-[30px]"
       >
-        <h3 className="uppercase xl:text-[26px] 2xl:text-[32px] font-[400] text-white">
+        <h3 className="uppercase xl:text-[26px] 2xl:text-[32px] lg:font-[400] text-white">
           Open to work
         </h3>
         <div className="relative h-[28px] w-[28px]">
@@ -18,7 +18,7 @@ const Availability = () => {
         </div>
       </div>
       <h4
-        className="font-[200] text-white lg:ml-1 based-in"
+        className="font-[200] text-white lg:ml-1"
       >
         Based in South Africa
       </h4>
